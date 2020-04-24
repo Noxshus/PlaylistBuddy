6f3fbc1dc2f6d8228f30731c43a9969d5c591cec
Noxshus <matthew@agius-muscat.net>
2020-04-24 10:15:29 +0200

    Testing Auth

--- requests.js
+++ requests.js
@@ -25,3 +25,28 @@ const fetchUsers = () => {
 };
 
 fetchUsers();
+
+function GetAuthorisation()
+{
+    app.get('/login', function(req, res) {
+        let scopes = 'user-read-private user-read-email';
+        res.redirect('https://accounts.spotify.com/authorize' +
+          '?response_type=code' +
+          '&client_id=' + my_client_id +
+          (scopes ? '&scope=' + encodeURIComponent(scopes) : '') +
+          '&redirect_uri=' + encodeURIComponent(redirect_uri));
+        });        
+}
+
+function RedirectToSpotifyAuth()
+{
+    let _clientId = "30f17f826d674bb48dcb9ae95ad228c3";
+    let _redirectUri = "https://noxshus.github.io/PlaylistBuddy/";
+    let _scopes = "user-read-private user-read-email";
+    
+    window.location.href = "https://accounts.spotify.com/authorize" +
+    "?client_id=" + encodeURIComponent(_clientId) +
+    "&response_type=token" +
+    "&redirect_uri=" + encodeURIComponent(_redirectUri) +
+    "&scope=" + encodeURIComponent(_scopes);
+}
