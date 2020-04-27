b4b7cce0f2624a60b5f9feece0d528bcb6956783
Noxshus <matthew@agius-muscat.net>
2020-04-27 09:37:44 +0200

    Trying out playlist functions

--- requests.js
+++ requests.js
@@ -8,7 +8,7 @@ function GetAuthorisation()
 {
     let _clientId = "30f17f826d674bb48dcb9ae95ad228c3";
     let _redirectUri = "https://noxshus.github.io/PlaylistBuddy/";
-    let _scopes = "user-modify-playback-state";
+    let _scopes = "user-modify-playback-state playlist-read-collaborative playlist-modify-public playlist-modify-private";
     
     window.location.href = "https://accounts.spotify.com/authorize" +
     "?client_id=" + encodeURIComponent(_clientId) +
@@ -20,12 +20,12 @@ function GetAuthorisation()
 function GetAuthorisationParameters()
 {
     const _urlParams = new URLSearchParams(window.location.hash);
-    accessToken = _urlParams.get('#access_token');
-    tokenType = _urlParams.get('token_type');
-    expiresIn = _urlParams.get('expires_in');
+    data.accessToken = _urlParams.get('#access_token');
+    data.tokenType = _urlParams.get('token_type');
+    data.expiresIn = _urlParams.get('expires_in');
 
     console.log(window.location.hash);
-    console.log("Access Token: " + accessToken + " Token Type: " + tokenType + " Expires In: " + expiresIn);
+    console.log("Access Token: " + data.accessToken + " Token Type: " + data.tokenType + " Expires In: " + data.expiresIn);
 }
 
 function PlayTrack()
@@ -34,10 +34,27 @@ function PlayTrack()
         method: "put",
         url: "https://api.spotify.com/v1/me/player/play",
         headers: {
-          "Authorization": "Bearer " + accessToken
+          "Authorization": "Bearer " + data.accessToken
         },
         data: { //this is the body
             "uris": ["spotify:track:4uLU6hMCjMI75M1A2tKUQC"]
         }
       })
+}
+
+function GetPlaylists()
+{
+    axios({
+        method: "get",
+        url: "https://api.spotify.com/v1/v1/me/playlists ",
+        headers: {
+          "Authorization": "Bearer " + data.accessToken
+        },
+      })
+      .then(function (response) {
+        console.log(response);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
 }
