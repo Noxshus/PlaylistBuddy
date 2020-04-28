f41ad32ca93d22757473fea1d418cf9b0d21240f
Noxshus <matthew@agius-muscat.net>
2020-04-28 16:03:59 +0200

    Save params between sessions + getting a new token when it's expired

---
+++ misc.js
@@ -0,0 +1,35 @@
+window.onload = function() {
+    Load(); //load user data, if it exists
+}
+
+function Save() 
+{
+    localStorage.setItem("playlistbuddy", JSON.stringify(userData));
+}
+
+function Load() 
+{
+    let _checkSave = JSON.parse(localStorage.getItem("playlistbuddy"));
+    if (_checkSave !== null) {
+        userData = _checkSave;
+    }
+}
+
+function SetTokenExpiry() //set the token expiry time to one hour from now
+{
+    userData.tokenExpiryTime = new Date();
+    userData.tokenExpiryTime.setHours(today.getHours() + 1);
+}
+
+function CheckIfTokenHasExpired() //access tokens only have a lifetime of 60 mins. After that point, user must re-auth and get a new one
+{
+    _timeNow = new Date();
+    if (_timeNow > userData.tokenExpiryTime)
+    {
+        return true; //has expired
+    }
+    else 
+    {
+        return false; //hasn't expired
+    }
+}
--- requests.js
+++ requests.js
@@ -1,106 +1,122 @@
-var userData = {
-  accessToken : "",
-  tokenType : "",
-  expiresIn : 0,
+var userData = { //saved between sessions to keep track of the token
+    accessToken : "",
+    tokenType : "",
+    expiresIn : 0,
 
-  userPlaylist: {},
-  userPlaylistTracks: {},
+    tokenExpiryTime: new Date(), //time the token will expire - defaults to the time right now (which would be the same as having an expired token)
+}
+
+var global = {
+    userPlaylist: {},
+    userPlaylistTracks: {},
 }
 
 window.onhashchange = function() { 
-  GetAuthorisationParameters();
-  console.log('New hash!');
+    GetAuthorisationParameters();
+    console.log('New hash!');
 }
 
 function GetAuthorisation()
 {
-  if(window.location.hash) 
-  { //if there's already a hash in the URL
-    GetAuthorisationParameters();
-  } 
-  else 
-  {
-    let _clientId = "30f17f826d674bb48dcb9ae95ad228c3";
-    let _redirectUri = "https://noxshus.github.io/PlaylistBuddy/";
-    let _scopes = "user-modify-playback-state playlist-read-collaborative playlist-modify-public playlist-modify-private playlist-read-private";
+    if(window.location.hash) 
+    { //if there's already a hash in the URL
+        if (CheckIfTokenHasExpired() == false)
+        { //if there is a hash, make sure it hasn't expired
+            GetAuthorisationParameters();
+        }
+    } 
+    else 
+    { //we need to get a new token
+        const _clientId = "30f17f826d674bb48dcb9ae95ad228c3";
+        const _redirectUri = "https://noxshus.github.io/PlaylistBuddy/";
+        const _scopes = "user-modify-playback-state playlist-read-collaborative playlist-modify-public playlist-modify-private playlist-read-private";
 
-    window.location.href = "https://accounts.spotify.com/authorize" +
-    "?client_id=" + encodeURIComponent(_clientId) +
-    "&response_type=token" +
-    "&redirect_uri=" + encodeURIComponent(_redirectUri) +
-    "&scope=" + encodeURIComponent(_scopes);
-  }
+        window.location.href = "https://accounts.spotify.com/authorize" +
+        "?client_id=" + encodeURIComponent(_clientId) +
+        "&response_type=token" +
+        "&redirect_uri=" + encodeURIComponent(_redirectUri) +
+        "&scope=" + encodeURIComponent(_scopes);
+    }
 }
 
 function GetAuthorisationParameters()
 {
-  const _urlParams = new URLSearchParams(window.location.hash);
-  userData.accessToken = _urlParams.get('#access_token');
-  userData.tokenType = _urlParams.get('token_type');
-  userData.expiresIn = _urlParams.get('expires_in');
+    if (window.location.hash != null) //check there's a hash before bothering
+    {
+        const _urlParams = new URLSearchParams(window.location.hash);
+        if (_urlParams != null)
+        {
+            userData.accessToken = _urlParams.get('#access_token');
+            userData.tokenType = _urlParams.get('token_type');
+            userData.expiresIn = _urlParams.get('expires_in');
 
-  console.log(window.location.hash);
-  console.log("Access Token: " + userData.accessToken + " Token Type: " + userData.tokenType + " Expires In: " + userData.expiresIn);
+            SetTokenExpiry(); //set the time the token is expected to expire at
+            Save(); //save it to local storage
+
+            console.log(window.location.hash);
+            console.log("Access Token: " + userData.accessToken + " Token Type: " + userData.tokenType + " Expires In: " + userData.expiresIn);
+        }
+    }
 }
 
 function PlayTrack()
 {
-  axios({
-      method: "put",
-      url: "https://api.spotify.com/v1/me/player/play",
-      headers: {
-        "Authorization": "Bearer " + userData.accessToken
-      },
-      data: { //this is the body
-          "uris": ["spotify:track:4uLU6hMCjMI75M1A2tKUQC"]
-      }
+    axios({
+        method: "put",
+        url: "https://api.spotify.com/v1/me/player/play",
+        headers: {
+            "Authorization": "Bearer " + userData.accessToken
+        },
+        data: { //this is the body
+            "uris": ["spotify:track:4uLU6hMCjMI75M1A2tKUQC"]
+        }
     })
 }
 
 function GetPlaylists()
 {
-  axios({
-      method: "get",
-      url: "https://api.spotify.com/v1/me/playlists",
-      headers: {
-        "Authorization": "Bearer " + userData.accessToken
-      },
-      params: {
-          "limit": 50, // this is the maximum value for this. Users with >50 playlists will need extra handling, using the "offset" param: https://developer.spotify.com/documentation/web-api/reference/playlists/get-a-list-of-current-users-playlists/
-      }
+    axios({
+        method: "get",
+        url: "https://api.spotify.com/v1/me/playlists",
+        headers: {
+            "Authorization": "Bearer " + userData.accessToken
+        },
+        params: {
+            "limit": 50, // this is the maximum value for this. Users with >50 playlists will need extra handling, using the "offset" param: https://developer.spotify.com/documentation/web-api/reference/playlists/get-a-list-of-current-users-playlists/
+        }
     })
     .then(function (response) {
-      //console.log(response);
-      //console.log(response.data);
-      userData.userPlaylist = response.data;
-      //console.log(userData.userPlayList);
-      UpdatePlaylists();
+        //console.log(response);
+        //console.log(response.data);
+        global.userPlaylist = response.data;
+        //console.log(global.userPlayList);
+        UpdatePlaylists();
     })
     .catch(function (error) {
-      console.log(error);
+        console.log(error);
     });
 }
 
 function GetTracks(_playlistId)
 {
-  axios({
-    method: "get",
-    url: "https://api.spotify.com/v1/playlists/" + _playlistId + "/tracks",
-    headers: {
-      "Authorization": "Bearer " + userData.accessToken
-    },
-    params: {
-      "fields": "items(track(name,artists,id))" //https://developer.spotify.com/documentation/web-api/reference/playlists/get-playlists-tracks/
-    } //this request supports drilling down and cherry-picking the return values. Since app might be used by people on data, better keep it as light as possible since these values are saved
-  })
-  .then(function (response) {
-    //console.log(response);
-    //console.log(response.data);
-    userData.userPlaylistTracks = response.data;
-    //console.log(userData.userPlaylistTracks);
-    UpdateTracks();
-  })
-  .catch(function (error) {
-    console.log(error);
-  });
+    axios({
+        method: "get",
+        url: "https://api.spotify.com/v1/playlists/" + _playlistId + "/tracks",
+        headers: {
+            "Authorization": "Bearer " + userData.accessToken
+        },
+        params: {
+            "fields": "items(track(name,artists,id))" //https://developer.spotify.com/documentation/web-api/reference/playlists/get-playlists-tracks/
+        } //this request supports drilling down and cherry-picking the return values. Since app might be used by people on data, better keep it as light as possible since these values are saved
+    })
+    .then(function (response) {
+        //console.log(response);
+        //console.log(response.data);
+        global.userPlaylistTracks = response.data;
+        //console.log(global.userPlaylistTracks);
+        UpdateTracks();
+    })
+    .catch(function (error) {
+        console.log(error);
+    });
 }
